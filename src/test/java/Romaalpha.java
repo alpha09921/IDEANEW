300b71aebe1afa369676e8f2f5499bdc567877ae
SeigniorGrom <167971982+SeigniorGrom@users.noreply.github.com>
2024-07-06 01:49:36 +0300

    Update Romaalpha.java

--- src/test/java/Romaalpha.java
+++ src/test/java/Romaalpha.java
@@ -1,2 +1,9 @@
 public class Romaalpha {
+   @Test
+    public void zela() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://macrosvit.com.ua/zelenskiy-vladimir-aleksandrovich/themes-718/");
+          }
 }
